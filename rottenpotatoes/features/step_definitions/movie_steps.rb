6cfb3ea96e38b19912615689f55312dcfd03721d
haoshuai999 <626593718@qq.com>
2020-04-08 21:52:04 -0400

    finish task 1 and 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -29,10 +29,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(",")
+  if uncheck.nil?
+    ratings.each do |rating|
+      check("ratings_" + rating.strip)
+    end
+  else
+    ratings.each do |rating|
+      uncheck("ratings_" + rating.strip)
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  page.all('tbody tr').count.should == 10
 end
